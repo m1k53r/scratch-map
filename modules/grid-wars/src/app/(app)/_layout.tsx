56a1ef17f9903c994a44d80d67b80b7c3a95bc50
m1k53r <mikobw369@protonmail.com>
2026-04-15 23:58:24 +0200

    add map

--- modules/grid-wars/src/app/(app)/_layout.tsx
+++ modules/grid-wars/src/app/(app)/_layout.tsx
@@ -7,6 +7,10 @@ export default function Layout() {
         <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
         <NativeTabs.Trigger.Icon sf="house.fill" md="home" />
       </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="profile">
+        <NativeTabs.Trigger.Label>Profile</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="person" md="person" />
+      </NativeTabs.Trigger>
       <NativeTabs.Trigger name="settings">
         <NativeTabs.Trigger.Label>Settings</NativeTabs.Trigger.Label>
         <NativeTabs.Trigger.Icon sf="gear" md="settings" />
--- modules/grid-wars/src/app/(app)/index.tsx
+++ modules/grid-wars/src/app/(app)/index.tsx
@@ -1,12 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
-import { authClient } from "@/lib/auth-client";
+import { StyleSheet, View } from "react-native";
+import MapView from "react-native-maps";
 
 export default function Index() {
-  const { data: session } = authClient.useSession();
-
   return (
     <View style={styles.container}>
-      <Text>Welcome, {session?.user.name}</Text>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 }
@@ -14,7 +20,9 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
