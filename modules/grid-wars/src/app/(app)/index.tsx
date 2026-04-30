79d4fa6898b9deb4c8d6c0bc4ed4dabe68d617d3
mickami742 <mickami742@gmail.com>
2026-04-30 23:55:02 +0200

    add area drawing feature

--- modules/grid-wars/src/app/(app)/index.tsx
+++ modules/grid-wars/src/app/(app)/index.tsx
@@ -1,24 +1,69 @@
-import { AppState, Image, Linking, Pressable, StyleSheet } from "react-native";
-import Mapbox from "@rnmapbox/maps";
+import {
+  Image,
+  Linking,
+  Pressable,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
+import Mapbox, { FillLayer } from "@rnmapbox/maps";
 import { useLocation } from "@/hooks/useLocation";
 import { authClient } from "@/lib/auth-client";
 import { useResume } from "@/hooks/useResume";
-import {
-  createTamagui,
-  View,
-  Button,
-  Tooltip,
-  Text,
-  Paragraph,
-  YStack,
-} from "tamagui";
+import { View, Button, Text, Input } from "tamagui";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import { useState, useRef } from "react";
+import { set } from "better-auth";
+import { Feature, Polygon, Point } from "geojson";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_API_KEY!);
 
 export default function Index() {
   const { data } = authClient.useSession();
   const { location, permissionStatus } = useLocation();
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const [selectMode, setSelectMode] = useState(false);
+  const [lobbyArea, setLobbyArea] = useState<Feature<Polygon> | null>(null);
+  const [points, setPoints] = useState<[number, number][]>([]);
+
+  let openCreateLobbyForm = () => {
+    setIsFormOpen((prev) => !prev);
+  };
+
+  const handleMapPress = (e: any) => {
+    if (!selectMode) return;
+
+    const coords = e?.geometry?.coordinates;
+    if (!coords) return;
+
+    setPoints((prev) => [...prev, coords]);
+  };
+
+  let drawLobbyArea = (e: any) => {
+    if (!location?.coords) return;
+
+    setIsFormOpen(false);
+
+    if (points.length >= 3) {
+      setLobbyArea({
+        type: "Feature",
+        geometry: {
+          type: "Polygon",
+          coordinates: [points],
+        },
+        properties: {},
+      });
+      setPoints([]);
+    }
+  };
+
+  if (!location) {
+    return (
+      <View>
+        <Text>Loading location...</Text>
+      </View>
+    );
+  }
 
   if (permissionStatus === "denied") {
     return (
@@ -31,7 +76,18 @@ export default function Index() {
 
   return (
     <View style={styles.container}>
-      <Mapbox.MapView style={styles.map} scaleBarEnabled={false}>
+      <Mapbox.MapView
+        style={styles.map}
+        scaleBarEnabled={false}
+        onPress={handleMapPress}
+      >
+        <Mapbox.Camera
+          zoomLevel={15}
+          centerCoordinate={[
+            location.coords.longitude,
+            location.coords.latitude,
+          ]}
+        />
         {location && (
           <Mapbox.MarkerView
             coordinate={[location.coords.longitude, location.coords.latitude]}
@@ -47,13 +103,102 @@ export default function Index() {
             </Pressable>
           </Mapbox.MarkerView>
         )}
+        {points && (
+          <Mapbox.ShapeSource
+            id="points-source"
+            shape={{
+              type: "FeatureCollection",
+              features: points.map((coords) => ({
+                type: "Feature",
+                geometry: {
+                  type: "Point",
+                  coordinates: coords,
+                },
+                properties: {},
+              })),
+            }}
+          >
+            <Mapbox.CircleLayer
+              id="points-layer"
+              style={{
+                circleRadius: 6,
+                circleColor: "red",
+                circleStrokeWidth: 2,
+                circleStrokeColor: "white",
+              }}
+            />
+          </Mapbox.ShapeSource>
+        )}
+        {lobbyArea && (
+          <Mapbox.ShapeSource id="source" shape={lobbyArea}>
+            <FillLayer
+              id="fill"
+              style={{
+                fillColor: "blue",
+                fillOpacity: 0.5,
+              }}
+            />
+          </Mapbox.ShapeSource>
+        )}
       </Mapbox.MapView>
-      <Button circular elevation="$4" size="$5" style={styles.fab}>
-        <Ionicons name="play" size={24} color="$theme" />
+      {isFormOpen ? (
+        <View style={styles.lobbyForm}>
+          <Input placeholder="Lobby name" width={200} margin="$2" />
+          <Input placeholder="Players number" width={200} margin="$2" />
+          <Button
+            style={styles.formButton}
+            onPress={() => {
+              setSelectMode(true);
+              setIsFormOpen(false);
+            }}
+          >
+            Select area
+          </Button>
+          <Button style={styles.formButton}>Create Lobby</Button>
+        </View>
+      ) : null}
+      {selectMode && (
+        <Button
+          onPress={() => {
+            drawLobbyArea(null);
+            setSelectMode(false);
+          }}
+          style={{ position: "absolute", top: 35, alignSelf: "center" }}
+          circular
+        >
+          <Ionicons name="checkmark" size={24} color="white" />
+        </Button>
+      )}
+      <Button
+        circular
+        elevation="$4"
+        size="$5"
+        style={styles.fab}
+        onPress={openCreateLobbyForm}
+      >
+        {isFormOpen ? (
+          <Ionicons name="close" size={24} color="white" />
+        ) : (
+          <Ionicons name="play" size={24} color="white" />
+        )}
+      </Button>
+      <Button
+        circular
+        elevation="$4"
+        size="$5"
+        style={styles.fabl}
+        onPress={() => {
+          setLobbyArea(null);
+          setPoints([]);
+        }}
+      >
+        <AntDesign name="clear" size={24} color="white" />
       </Button>
     </View>
   );
 }
+
+const { width, height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
@@ -82,5 +227,30 @@ const styles = StyleSheet.create({
     position: "absolute",
     right: 16,
     bottom: 16,
+  },
+  fabl: {
+    position: "absolute",
+    left: 16,
+    bottom: 16,
+  },
+  lobbyForm: {
+    position: "absolute",
+    bottom: 80,
+    width: "100%",
+    alignContent: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "black",
+    padding: 16,
+    borderRadius: 8,
+    maxWidth: width - 10,
+    minHeight: height - height / 5,
+  },
+  formButton: {
+    margin: 8,
+    width: width / 2,
+  },
+  lobbyArea: {
+    backgroundColor: "rgba(255, 0, 0, 0.5)",
   },
 });
